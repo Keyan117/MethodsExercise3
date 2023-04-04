d4a9305f689bbe5d12fdcba787907f5016517e29
Keyan117 <kdaneshvari98@gmail.com>
2023-04-03 22:51:53 -0700

    update

--- MethodsExercise3/Program.cs
+++ MethodsExercise3/Program.cs
@@ -7,7 +7,42 @@ namespace MethodsExercise3
     {
         static void Main(string[] args)
         {
-
+            
         }
-    }
-}
+        //Write a method that will print
+        //to the console all numbers 1000 through - 1000.
+        public static void PrintRange(int celing, int floor)
+        {
+            for (int i = celing; i >= floor; i--)
+            {
+                Console.WriteLine(i);
+            }
+        }
+        //Write a method that
+        //Will print to the console numbers 3 through 999 by 3 each time.
+        public static int
+        {
+            for(int k = 3; k = < 999; k + 3);
+            {
+                Console.WriteLine(k);
+            }
+            //Write a method to accept two
+            //integers as parameters and check whether they are equal or not.
+            static int bool IsEquals (int a, int b)
+            {
+                //how do I determine if two numbers are the same?
+                if(a == b)
+                {
+                    return true;
+                }
+                else
+                {
+                    return false;
+                }
+                
+                }
+                }
+            }
+        }
+
+        
